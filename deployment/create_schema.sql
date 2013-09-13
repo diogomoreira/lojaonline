262dfd92c4f0a37e346529a38a1170ce6f63180b
Diogo Moreira <diogo.dmoreira@gmail.com>
2013-09-12 23:32:56 -0300

    Changing primary key to serial type

--- deployment/create_schema.sql
+++ deployment/create_schema.sql
@@ -1,13 +1,14 @@
 CREATE TABLE loja_categorias (
-	codigo INT8 NOT NULL,
+	codigo SERIAL,
 	nome VARCHAR(50) NOT NULL,
 	PRIMARY KEY (codigo)
 );
 
 CREATE TABLE loja_produtos (
-	codigo INT8 NOT NULL,
+	codigo SERIAL,
 	nome VARCHAR(255) NOT NULL,
 	preco NUMERIC(8,2) NOT NULL,
 	cod_categoria INT8 NOT NULL,
+	PRIMARY KEY (codigo),
 	FOREIGN KEY(cod_categoria) REFERENCES loja_categorias(codigo)
 );
